395e2a5e91ad0a7041308891eee1f884f3ba6e80
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-04-26 20:56:25 -0600

    how to trigger the web page

---
+++ ponder/dom.js
@@ -0,0 +1,38 @@
+// JavaScript for the DOM page of the Ponder project
+const title = document.querySelector('h1');
+// Log the title element to the console
+console.log(title);
+// Change the text content of the title
+title.textContent = 'Web Page Components';
+
+//document.querySelector('#topics').style.color = 'red';
+let topics = document.querySelector('#topics');
+//topics.style.color = 'red';
+
+document.getElementById('topics').style.color = 'purple';
+
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+
+//para.style.backgroundColor = 'lightblue';
+para.classList.add('background');
+
+//document.querySelector('body').classList.add('background');
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'images/web.png');
+
+
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    //document.getElementById(codeValue).style.color = 'red';
+})
+                
