39fd33dcbca4292baed948f3af5f46c1422f0140
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-05-02 05:54:29 -0600

    Update index.js
    Some meat will have to test later

--- mission/index.js
+++ mission/index.js
@@ -7,8 +7,10 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
+        document.body.classList.add('dark');// Dark
         // code for changes to colors and logo
     } else {
+        document.body.classList.remove('dark'); // White and bright
         // code for changes to colors and logo
     }
 }           
