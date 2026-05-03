5344aab971f7af76e769185e2493559b9aac359a
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-05-02 22:54:48 -0600

    :p

--- mission/index.js
+++ mission/index.js
@@ -5,19 +5,18 @@ let logo = document.querySelector('img');
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
-    let current = selectElem.value;
-    if (current == 'dark') {
-        // code for changes to colors and logo
-        document.body.classList.add('dark');
-        //https://wddbyui.github.io/wdd131/images/byui-logo-white.png
-        logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png');
-        //logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png';
-    } else {
-        // code for changes to colors and logo
-        document.body.classList.remove('dark'); // White and bright
-        logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
-        //logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp';
-        
-    }
-}           
-                    
+        let current = selectElem.value;
+        if (current == 'dark') {
+                // code for changes to colors and logo
+                document.body.classList.add('dark');
+                //https://wddbyui.github.io/wdd131/images/byui-logo-white.png
+                logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png');
+                //logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png';
+            } else {
+                // code for changes to colors and logo
+                document.body.classList.remove('dark'); // White and bright
+                logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
+                //logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp';
+
+            }
+        }
