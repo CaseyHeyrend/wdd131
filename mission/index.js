657dcbfe1442b2a9514bc289e7e7771e0dc8ec7e
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-05-05 20:29:42 -0600

    no web dresses

--- mission/index.js
+++ mission/index.js
@@ -2,21 +2,19 @@
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
 
-selectElem.addEventListener('change', changeTheme);
+
 
 function changeTheme() {
         let current = selectElem.value;
         if (current == 'dark') {
                 // code for changes to colors and logo
                 document.body.classList.add('dark');
-                //https://wddbyui.github.io/wdd131/images/byui-logo-white.png
-                logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png');
-                //logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png';
+                logo.setAttribute('src', '/images/byui-logo-white.png');
             } else {
                 // code for changes to colors and logo
                 document.body.classList.remove('dark'); // White and bright
-                logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
-                //logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp';
+                logo.setAttribute('src', '/images/byui-logo-blue.webp');
 
             }
         }
+selectElem.addEventListener('change', changeTheme);
