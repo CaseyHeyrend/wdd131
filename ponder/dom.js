81c899427b06acc8957d7fdb8ae94e1095621f9a
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-05-08 00:44:21 -0600

    Ugh

--- ponder/dom.js
+++ ponder/dom.js
@@ -24,7 +24,7 @@ para.classList.add('background');
 //document.querySelector('body').classList.add('background');
 
 const image = document.querySelector('img');
-image.setAttribute('src', '/ponder/unit1/images/web.png');
+image.setAttribute('src', 'images/web.png');
 
 
 
