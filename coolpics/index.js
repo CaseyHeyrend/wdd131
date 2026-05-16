4bf343fd4808ec5fd638cae2c3e7cac27d243539
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-05-15 18:12:44 -0600

    Ponder is Helpgul

--- coolpics/index.js
+++ coolpics/index.js
@@ -1,7 +1,30 @@
 const menuButton = document.getElementById('menu-button');
 const navMenu = document.getElementById('nav-menu');
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
 
 menuButton.addEventListener('click', () => {
     navMenu.classList.toggle('open');
 });
 
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    if (e.target.tagName === 'IMG') {
+        modalImage.src = e.target.src;
+        modal.showModal();
+    }
+}
+
+// Event listener for the close button
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
