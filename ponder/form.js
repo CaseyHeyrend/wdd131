254346f5e2e5fed5404c9408449c579fa22ff378
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-06-05 22:06:56 -0600

    Add validation messages for campus selection and travel notes in form submission

--- ponder/form.js
+++ ponder/form.js
@@ -8,7 +8,7 @@ const campusBoxes = document.querySelectorAll('input[name="campus"]');
 
 function updateNotesField() {
   const value = travelRange.value;
-  
+
 
   // Show the travel notes on the form if they are choosing many campuses and require it
   
@@ -46,12 +46,23 @@ form.addEventListener("submit", function (event) {
 
   // Validate the input
   // Let the user know to select at least one campus
+  if (selectedCampuses === 0) {
+    output.textContent = "Please select at least one campus.";
+    return;
+  }
 
-  
   // Let the user know if they choose many campuses but didn't put a note that they need to add a note
-
+  if (type === "many" && !note) {
+    output.textContent = "Please add a note travel note.";
+    return;
+  }
   
-  //Let the user know if they choose many campus but only had one campus selected that they need to choose at least two campuses
+  //Let the user know if they choose many campus but only had one campus selected that they 
+  // need to choose at least two campuses
+  if (type === "many" && selectedCampuses.length < 2) {
+    output.textContent = "Please select at least two campuses";
+    return;
+  }
   
 
   if (isPastDate(availableDate)) {
