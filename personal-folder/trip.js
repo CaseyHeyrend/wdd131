b6dc496b932825af4c3bf9cbee6627dcf12d03cc
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-07-09 16:54:02 -0600

    Update logo and favicon; remove old logo file and add new favicon

--- personal-folder/trip.js
+++ personal-folder/trip.js
@@ -1,42 +1,38 @@
-const trip = [
+const trips = [
     {
-        name: "Yellowstone National Park",
-        stub: "yellowstone",
-        imgSrc: "images/yellowstone.jpg", // Photo by James Lee,
+        name: "Yellowstone Adventure",
+        destination: "Yellowstone National Park",
+        imgSrc: "images/yellowstone.jpg", // Photo by James Lee
         imgAlt: "Image of Yellowstone National Park",
         description: "Yellowstone National Park is a vast wilderness area in the U.S. states of Wyoming, Montana, and Idaho. It is known for its wildlife and geothermal features, especially Old Faithful geyser.",
         availability: "Open year-round, but some areas may be inaccessible during winter months.",
-        campers: 4,
-        type: ["Tent camping", "RV camping", "Backcountry camping", "Cabin rentals"]
+        type: ["Tent", "RV", "Cabin"]
     },
     {
-        name: "Teton Valley",
-        stub: "teton-valley",
-        imgSrc: "images/tetons.jpg", // Photo by Wal Saravia,
+        name: "Teton Valley Getaway",
+        destination: "Teton Valley",
+        imgSrc: "images/tetons.jpg", // Photo by Wal Saravia
         imgAlt: "Image of Teton Valley",
         description: "Teton Valley is a picturesque region in western Wyoming, known for its stunning mountain views, outdoor recreation opportunities, and charming small towns.",
         availability: "Open year-round, with peak tourist season in summer.",
-        campers: 2,
-        type: ["Tent", "RV camping", "Cabin rentals", "backcountry camping"]
+        type: ["Tent", "RV", "Cabin"]
     },
     {
-        name: "Uinta Mountains",
-        stub: "uinta-mountains",
-        imgSrc: "/images/elizabeth_ridge-CaseyHeyrend.jpg", // Photo by Casey Heyrend,
+        name: "Uinta Mountains Retreat",
+        destination: "Uinta Mountains",
+        imgSrc: "images/elizabeth_ridge-CaseyHeyrend.jpg", // Photo by Casey Heyrend
         imgAlt: "Image of Uinta Mountains",
         description: "The Uinta Mountains are a range of mountains in northeastern Utah, known for their alpine lakes, hiking trails, and diverse wildlife.",
         availability: "Open year-round, with peak tourist season in summer.",
-        campers: 6,
-        type: ["Tent camping", "RV camping", "Backcountry camping", "Cabin rentals"]
+        type: ["Tent", "RV", "Cabin"]
     },
     {
-        name: "Bear Lake",
-        stub: "bear-lake",
-        imgSrc: "#",
+        name: "Bear Lake Getaway",
+        destination: "Bear Lake",
+        imgSrc: "images/", // 
         imgAlt: "Image of Bear Lake",
-        description: "Bear Lake is a natural freshwater lake located on the Utah-Idaho border, known for its turquoise-blue water and recreational activities such as boating, fishing, and swimming.",
+        description: "Bear Lake is a natural freshwater lake located on the Utah-Idaho border, known for its turquoise-blue water, sandy beaches, and recreational activities such as boating, fishing, and swimming.",
         availability: "Open year-round, with peak tourist season in summer.",
-        campers: 6,
-        type: ["Tent camping", "RV camping", "Cabin rentals"]
+        type: ["Tent", "RV", "Cabin"]
     }
-]
+];
