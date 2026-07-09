2c1a1c4e9b2b4b75f4b09d00d35a91611bcac95c
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2026-07-08 23:17:36 -0600

    Add Bear Lake Getaway trip details and update image comments for consistency

--- personal-folder/index.js
+++ personal-folder/index.js
@@ -3,7 +3,7 @@ const trips = [
     {
         name: "Yellowstone Adventure",
         destination: "Yellowstone National Park",
-        imgSrc: "images/yellowstone.jpg", // Photo by James Lee,
+        imgSrc: "images/yellowstone.jpg", // Photo by James Lee
         imgAlt: "Image of Yellowstone National Park",
         description: "Yellowstone National Park is a vast wilderness area in the U.S. states of Wyoming, Montana, and Idaho. It is known for its wildlife and geothermal features, especially Old Faithful geyser.",
         availability: "Open year-round, but some areas may be inaccessible during winter months.",
@@ -12,7 +12,7 @@ const trips = [
     {
         name: "Teton Valley Getaway",
         destination: "Teton Valley",
-        imgSrc: "images/tetons.jpg", // Photo by Wal Saravia,
+        imgSrc: "images/tetons.jpg", // Photo by Wal Saravia
         imgAlt: "Image of Teton Valley",
         description: "Teton Valley is a picturesque region in western Wyoming, known for its stunning mountain views, outdoor recreation opportunities, and charming small towns.",
         availability: "Open year-round, with peak tourist season in summer.",
@@ -21,9 +21,18 @@ const trips = [
     {
         name: "Uinta Mountains Retreat",
         destination: "Uinta Mountains",
-        imgSrc: "images/elizabeth_ridge-CaseyHeyrend.jpg", // Photo by Casey Heyrend,
+        imgSrc: "images/elizabeth_ridge-CaseyHeyrend.jpg", // Photo by Casey Heyrend
         imgAlt: "Image of Uinta Mountains",
         description: "The Uinta Mountains are a range of mountains in northeastern Utah, known for their alpine lakes, hiking trails, and diverse wildlife.",
+        availability: "Open year-round, with peak tourist season in summer.",
+        type: ["Tent", "RV", "Cabin"]
+    },
+    {
+        name: "Bear Lake Getaway",
+        destination: "Bear Lake",
+        imgSrc: "images/", // 
+        imgAlt: "Image of Bear Lake",
+        description: "Bear Lake is a natural freshwater lake located on the Utah-Idaho border, known for its turquoise-blue water, sandy beaches, and recreational activities such as boating, fishing, and swimming.",
         availability: "Open year-round, with peak tourist season in summer.",
         type: ["Tent", "RV", "Cabin"]
     }
